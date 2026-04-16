75262737155a4a44988ee595930747a167ccfcfe
Fabio Joaquim de Sousa <fabiojsousa91@gmail.com>
2026-04-16 19:50:40 -0300

    carrinho de produtos

--- 3.Return.js
+++ 3.Return.js
@@ -7,6 +7,35 @@ todos os produtos acima de R$ 30,00 tem 10% de desconto
 
 const cart = [10, 244, 99, 2, 20, 33, 250]
 
-for (let i = 0; i > 30; i++) {
+let finalValueWithDiscount = 0
+let finalValueWithoutDiscount = 0
+
+
+    cart.forEach(value => {
+        finalValueWithoutDiscount = finalValueWithoutDiscount + value
+        return finalValueWithoutDiscount
+    });
+
+    /* 
+        R$ 250,00 // 7% -> R$ 17,50
+        R$ 250,00 * 7 -> 1750,00 / 100 -> R$ 17,50
     
-}
+    */
+
+    function calculateDiscount(price, discount) {
+        const result = price * discount / 100
+        return result
+    }
+
+    cart.forEach(value => {
+        if (value > 30) {
+            const discount = calculateDiscount(value, 10)
+            finalValueWithDiscount = finalValueWithDiscount + (value - discount)
+        } else {
+            finalValueWithDiscount = finalValueWithDiscount + value
+        }
+    });
+
+    console.log(`O valor final da compra foi de R$ ${finalValueWithoutDiscount.toFixed(2)},
+porém, você teve desconto, irá pagar apenas R$ ${finalValueWithDiscount.toFixed(2)}, ou seja,
+você economizou R$ ${(finalValueWithDiscount - finalValueWithoutDiscount).toFixed(2)}`)
